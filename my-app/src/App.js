15034bff95e8679614de6988e296d15d39b11f7f
trpowerr <mixazbubl@yandex.ru>
2020-02-20 11:15:58 +0300

    turn-to

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+
+function App() {
+  return (
+      <div>
+        <h1>Hello world</h1>
+        <Button variant='primary'>Button</Button>
+      </div>
+
+    
+  );
+}
+
+export default App;
