aae394002703048c008c33c36ef9558799242dd8
trpowerr <mixazbubl@yandex.ru>
2020-02-21 13:27:21 +0300

    add react router

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import {
   Navbar,
   Container,
@@ -8,6 +9,14 @@ import {
   Button
 } from 'react-bootstrap';
 import logo from './logo192.png'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from '../Pages/Home';
+import Contacts from '../Pages/Contacts';
+import About from '../Pages/About';
+import Blog from '../Pages/Blog';
+
 
 export default class Header extends Component {
   render() {
@@ -27,10 +36,10 @@ export default class Header extends Component {
               <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
               <Navbar.Collapse id='responsive-navbar-nav'>
                 <Nav className='mr-auto'>
-                  <Nav.Link href='/'>HOME</Nav.Link>
-                  <Nav.Link href='/about'>About us</Nav.Link>
-                  <Nav.Link href='/contacts'>Contacts</Nav.Link>
-                  <Nav.Link href='/blog'>Blog</Nav.Link>
+                  <Nav.Link href='/'> Home </Nav.Link>
+                  <Nav.Link href='/about'> About us </Nav.Link>
+                  <Nav.Link href='/contacts'> Contacts </Nav.Link>
+                  <Nav.Link href='/blog'> Blog </Nav.Link>
                 </Nav>
                 <Form inline>
                   <FormControl
@@ -45,6 +54,15 @@ export default class Header extends Component {
 
           </Container>
         </Navbar>
+
+        <Router>
+          <Switch>
+            <Route  exact path='/' component={Home} />
+            <Route  exact path='/about' component={About} />
+            <Route  exact path='/contacts' component={Contacts} />
+            <Route  exact path='/blog' component={Blog} />
+          </Switch>
+        </Router>
       </>
     );
   }
