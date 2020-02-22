56981fe1b42855682aa05fe6888c54c0de858854
trpowerr <mixazbubl@yandex.ru>
2020-02-22 20:11:54 +0300

    contact and cards

--- my-app/src/Pages/Contacts.js
+++ my-app/src/Pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 
 export default class Contacts extends Component {
   render() {
     return (
-      <div>
-        Contacts
-      </div>
+      <Container style={{width: '500px'}}>
+        <h1 className='text-center'> Contact us </h1>
+        <Form>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email adress</Form.Label>
+            <Form.Control type='email' placeholder='Enter email'/>
+            <Form.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Longum est enim
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Example text area</Form.Label>
+            <Form.Control as='textarea' rows='3'/>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicCheckbox'>
+            <Form.Check type='checkbox' label='Check me out'/>
+          </Form.Group>
+          <Button variant='primary' type='submit' >Submit</Button>
+
+        </Form>
+      </Container>
     )
   }
 }
--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import CarouselBox from '../Components/CarouselBox';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container, CardDeck, Card, Button } from 'react-bootstrap';
 
 export default class Home extends Component {
   render() {
     return (
+      <>
       <CarouselBox />
+      <Container>
+        <h2 className='text-center m-4'>Our Team</h2>
+        <CardDeck className='m-4'>
+          <Card>
+            <Card.Img 
+              variant='top'
+              src='https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+            />
+            <Card.Body>
+              <Card.Title>Developers</Card.Title>
+              <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Omnes enim iucundum motum, quo sensus hilaretur. Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.
+              </Card.Text>
+              <Button variant='primary'>About team</Button>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img 
+              variant='top'
+              src='https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+            />
+            <Card.Body>
+              <Card.Title>Developers</Card.Title>
+              <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Omnes enim iucundum motum, quo sensus hilaretur. Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.
+              </Card.Text>
+              <Button variant='primary'>About team</Button>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img 
+              variant='top'
+              src='https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+            />
+            <Card.Body>
+              <Card.Title>Developers</Card.Title>
+              <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Omnes enim iucundum motum, quo sensus hilaretur. Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.
+              </Card.Text>
+              <Button variant='primary'>About team</Button>
+            </Card.Body>
+          </Card>
+
+        </CardDeck>
+      </Container>
+      </>
     )
   }
 }
